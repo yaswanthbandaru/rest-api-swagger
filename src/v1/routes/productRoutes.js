fda87be5ee0341416effc3c7843a8153f1ec22eb
Bandaru Yaswanth <yaswanthbandaru424@gmail.com>
2025-02-06 11:41:51 +0530

    Added swagger jsdoc

--- src/v1/routes/productRoutes.js
+++ src/v1/routes/productRoutes.js
@@ -28,7 +28,58 @@ const checkAuth = require('../authentication/checkAuth');
  *                              type: object
  */
 router.get('/', productController.getAllProducts);
+
+/**
+ * @swagger
+ * /v1/products:
+ *  post:
+ *      summary: Create a product
+ *      description: Create a product into Database
+ *      tags: [Products]
+ *      responses:
+ *          200:
+ *              description: Product is created
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          proporties:
+ *                              name: productId
+ *                              type: string
+ */
 router.post('/', checkAuth, productController.createProduct);
+
+/**
+ * @swagger
+ * /v1/products/{productId}:
+ *  get:
+ *      summary: Get a Product
+ *      description: Retrieve a single product by productId
+ *      tags: [Products]
+ *      parameters:
+ *        - in: path
+ *          name: productId
+ *          required: true
+ *          schema:
+ *            type: string
+ *          description: The product ID
+ *      responses:
+ *          200:
+ *              description: A single product
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              productId:
+ *                                  type: string
+ *                              name:
+ *                                  type: string
+ *                              price:
+ *                                  type: number
+ *                              description:
+ *                                  type: string
+ */
 router.get('/:productId', productController.getProductById);
 router.patch('/:productId', checkAuth, productController.updateProductById);
 router.delete('/:productId', checkAuth, productController.deleteProductById);
